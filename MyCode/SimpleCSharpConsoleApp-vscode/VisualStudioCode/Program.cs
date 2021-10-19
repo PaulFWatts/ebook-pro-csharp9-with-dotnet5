e9f52dc3f8d24ff4923401487f32fa3b0c039c6f
Paul Watts <7057417+PaulFWatts@users.noreply.github.com>
2021-10-19 18:17:41 +1100

    Added vscode source and configuration files

---
+++ MyCode/SimpleCSharpConsoleApp-vscode/VisualStudioCode/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SimpleCSharpConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Set up Console UI (CUI);
+            Console.Title = "My Rocking App";
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("*************************************");
+            Console.WriteLine("***** Welcome to My Rocking App *****");
+            Console.WriteLine("*************************************");
+            Console.BackgroundColor = ConsoleColor.Black;    
+        }
+    }
+}
